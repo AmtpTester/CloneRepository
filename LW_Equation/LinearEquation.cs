bf2c066ce5859ba3260a41025fa01f850a864e8d
Vladislau Zhuk <86854997+SnikersFar@users.noreply.github.com>
2022-05-28 21:04:10 +0300

    upd. operator +

--- LW_Equation/LinearEquation.cs
+++ LW_Equation/LinearEquation.cs
@@ -28,7 +28,7 @@ namespace LW_Equation
         static public LinearEquation operator +(LinearEquation first, float second)
         {
             LinearEquation equation = first;
-            equation.coefficients[0] *= second;
+            equation.coefficients[0] += second;
             return equation;
         }
         static public LinearEquation operator -(LinearEquation first, float second)
